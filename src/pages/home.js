b63c3e7121fbd905c5e1adadb110b51aa8966db4
Pawel Wujczyk <pwujczyk@hotmail.com>
2020-12-01 15:27:46 +0100

    moment

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import apiService from '../services/apiService'
+import Moment from 'react-moment';
 
 
 function Home() {
@@ -8,10 +9,10 @@ function Home() {
     const [filter,setFilter]=useState({name:"",company:""})
 
     useEffect(() => {
-        getSalaries();
+        getSalaries(filter);
     }, []);
 
-    async function getSalaries() {
+    async function getSalaries(filter) {
         const r = await apiService.getSalaries(filter);
         setSalaries(r);
     }
@@ -23,47 +24,53 @@ function Home() {
             </div>
             <div>
                 <table>
+                    <thead>
                     <tr>
                         <th>[SalaryId]</th>
                         <th>[Position]</th>
                         <th>[Value]</th>
-                        <th>[Comment]</th>
-                        <th>[Company]</th>
-                        <th>[ValueConfirmed]</th>
-                        <th>[CreationDate]</th>
-                        <th>[Name]</th>
-                        <th>[Source]</th>
-                        <th>[Expectation]</th>
                         <th>[B2b]</th>
+                        <th>[Company]</th>
+                        <th>[CreationDate]</th>
+                        <th>[Source]</th>
+                        <th>[Comment]</th>
+                        <th>[ValueConfirmed]</th>                 
+                        <th>[Name]</th>                
+                        <th>[Expectation]</th>
+                        
                     </tr>
                     <tr>
                         <td>salaryId</td>
                         <td>position</td>
                         <td>value</td>
-                        <td>comment</td>
-                        <td><input type="text" value={filter.company} onChange={e => setFilter({company: e.target.value})}/></td>
-                        <td>.valueConfirmed</td>
-                        <td>creationDate</td>
-                        <td><input type="text" value={filter.name} onChange={e => setFilter({name: e.target.value})}/></td>
-                        <td>source</td>
-                        <td>expectation</td>
                         <td>b2b</td>
+                        <td><input type="text" value={filter.company} onChange={e => setFilter({company: e.target.value})}/></td>
+                        <td>creationDate</td>
+                        <td>source</td>
+                        <td>comment</td>
+                        <td>.valueConfirmed</td>
+                        <td><input type="text" value={filter.name} onChange={e => setFilter({name: e.target.value})}/></td>
+                        <td>expectation</td>
+                       
                     </tr>
+                    </thead>
+                    <tbody>
                     {salaries.map(item => (
-                        <tr>
+                        <tr key={item.salaryId}>
                             <td>{item.salaryId}</td>
                             <td>{item.position}</td>
                             <td>{item.value}</td>
-                            <td>{item.comment}</td>
-                            <td>{item.company}</td>
-                            <td>{item.valueConfirmed}</td>
-                            <td>{item.creationDate}</td>
-                            <td>{item.name}</td>
-                            <td>{item.source}</td>
-                            <td>{item.expectation}</td>
                             <td>{item.b2b?<p>Yes</p>:<p>No</p>}</td>
+                            <td>{item.company}</td>
+                            <td><Moment format="YYYY.MM.DD">{item.creationDate}</Moment></td>
+                            <td>{item.source}</td>
+                            <td>{item.comment}</td>
+                            <td>{item.valueConfirmed}</td>
+                            <td>{item.name}</td>
+                            <td>{item.expectation}</td>                
                         </tr>
                     ))}
+                    </tbody>
                 </table>
             </div>
         </div>
