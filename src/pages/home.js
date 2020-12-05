ff8f448bae763a65d7d0c4e85a34c680912eda71
Pawel Wujczyk <pwujczyk@hotmail.com>
2020-12-05 17:44:21 +0100

    table format

--- src/pages/home.js
+++ src/pages/home.js
@@ -6,19 +6,20 @@ import Button from '@material-ui/core/Button';
 import IconButton from "@material-ui/core/IconButton";
 import DeleteIcon from "@material-ui/icons/Delete";
 import { useConfirm } from 'material-ui-confirm';
-
+import { useHistory } from 'react-router-dom'
 
 function Home() {
 
     const [salaries, setSalaries] = useState([])
     const [filter, setFilter] = useState({ name: "", company: "" })
+    const history = useHistory()
     const confirm = useConfirm();
 
     const handleDelete = (item) => {
         confirm({ description: 'This will delete salary record from database.' })
             .then(() => {
-                 apiService.removeSalary(item.salaryId); 
-                })
+                apiService.removeSalary(item.salaryId);
+            })
             .catch(() => { /* ... */ });
     };
 
@@ -34,15 +35,22 @@ function Home() {
         }))
     }
 
+    const newSalary = () => {
+        history.push('/Add')
+    }
+
     async function getSalaries(filter) {
         const r = await apiService.getSalaries(filter);
         setSalaries(r);
     }
 
+
+
     return (
         <div>
             <div>
-                <button onClick={() => getSalaries(filter)}>Get</button>
+                <Button onClick={() => getSalaries(filter)} color="primary" variant="contained">Update list</Button>
+                <Button onClick={() => newSalary()} color="primary" variant="contained">New</Button>
             </div>
             <div>
                 <table>
@@ -54,7 +62,12 @@ function Home() {
                         </tr>
                         <tr>
                             {fields.map((f) => {
-                                return (<td><input type={f.Type} name={f.Name} value={filter[f.field]} onChange={applyFilters} /></td>)
+                                if (f.filtered) {
+                                    return (<td><input type={f.Type} name={f.Name} value={filter[f.field]} onChange={applyFilters} /></td>)
+                                }
+                                else {
+                                    return (<td>{f.Label}</td>)
+                                }
                             })}
                         </tr>
                     </thead>
@@ -62,7 +75,18 @@ function Home() {
                         {salaries.map(item => (
                             <tr key={item.salaryId}>
                                 {fields.map((f) => {
-                                    return <td>{item[f.field]}</td>
+                                    if (f.Type == "date") {
+                                        return <td><Moment format="YYYY.MM.DD">{item[f.field]}</Moment></td>
+                                    }
+                                    else {
+                                        if (f.Type == "checkbox") {
+                                            debugger;
+                                            return <td>{Number(item.b2b)}</td>
+                                        }
+                                        else {
+                                            return <td>{item[f.field]}</td>
+                                        }
+                                    }
                                 })}
                                 <td>
                                     <IconButton onClick={() => handleDelete(item)}>
