1803b791139c0cba1e43ddc055eb92182af8036d
Pawel Wujczyk <pwujczyk@hotmail.com>
2023-03-26 21:46:16 +0200

    firebase

---
+++ src/firebase.js
@@ -0,0 +1,69 @@
+import { initializeApp } from "firebase/app";
+import { isJwtExpired } from 'jwt-check-expiration';
+
+import {
+    GoogleAuthProvider,
+    getAuth,
+    signInWithPopup,
+    signOut,
+} from "firebase/auth";
+
+const firebaseConfig = {
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.AUTH_DOMAIN,
+    projectId: process.env.PROJECT_ID,
+    storageBucket: process.env.STORAGE_BUCKET,
+    messagingSenderId: process.env.MESSAGING_SENDER_ID,
+    appId: process.env.APP_ID,
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+
+const googleProvider = new GoogleAuthProvider();
+const signInWithGoogle = async () => {
+    try {
+        const res = await signInWithPopup(auth, googleProvider);
+        console.log(res);
+        localStorage.setItem("token", res.user.accessToken);
+
+        console.log("token validation");
+        let token = localStorage.getItem('token');
+       // console.log("token from localstorage", token);
+        return res.user;
+    } catch (err) {
+        console.error(err);
+        alert(err.message);
+    }
+};
+
+const logout = () => {
+    signOut(auth);
+    localStorage.removeItem("token")
+};
+
+const getToken = () => {
+    let token = localStorage.getItem('token');
+    return token;
+}
+
+const tokenExpired = () => {
+    let token = localStorage.getItem('token');
+    if (token) {
+        let result = isJwtExpired(token)
+        return result;
+    }
+    else {
+        return true;
+    }
+}
+
+export {
+    auth,
+    signInWithGoogle,
+    logout,
+    getToken,
+    tokenExpired
+};
