6a72ed6b10786a16eb4bd31583062cf837acc6da
Pawel Wujczyk <pwujczyk@hotmail.com>
2023-03-27 20:47:07 +0200

    auth

--- src/components/Session/index.js
+++ src/components/Session/index.js
@@ -1,15 +1,27 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { auth, signInWithGoogle,logout } from "../../firebase.js";
+import { useAuthState } from "react-firebase-hooks/auth";
+
 
 export default function Session(props) {
 
+    const [user,loading]=useAuthState(auth);
+    useEffect(()=>{
+        console.log("not sure");
+    },[user,loading])
+
+    const login=()=>{
+        signInWithGoogle();
+        debugger;
+    }
 
     return (
         <span>
-            <button onClick={signInWithGoogle}>Login</button>
+            <button onClick={login}>Login</button>
             <button onClick={logout}>Logout</button>
-            <span>User:{auth.user ? auth.user.email : "is missing in session"}</span>
-            <div>{auth.user  ? props.children : "child not rendered"}</div>
+            <span>User:{auth?.currentUser ? auth?.currentUser?.displayName : "is missing in session"}</span>
+            <span>{auth?.currentUser?.email}</span>
+            <div>{auth?.currentUser  ? props.children : "child not rendered"}</div>
         </span>
     )
 }
--- src/firebase.js
+++ src/firebase.js
@@ -8,18 +8,17 @@ import {
     signOut,
 } from "firebase/auth";
 
-console.log(process.env.REACT_APP_API_KEY)
-debugger;
-const firebaseConfig = {
-    apiKey: process.env.REACT_APP_API_KEY,
-    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-    projectId: process.env.REACT_APP_PROJECT_ID,
-    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-    appId: process.env.REACT_APP_APP_ID,
-};
 
+const firebaseConfig = {
+    apiKey: "AIzaSyC9jrzURk6uuduwysaQDHaGEIoEvYv2ibk",
+    authDomain: "ptsalariesprod.firebaseapp.com",
+    projectId: "ptsalariesprod",
+    storageBucket: "ptsalariesprod.appspot.com",
+    messagingSenderId: "296196078902",
+    appId: "1:296196078902:web:aa03fe076e07069e19c797"
+  };
 // Initialize Firebase
+console.log(firebaseConfig)
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 
